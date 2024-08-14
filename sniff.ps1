f6d0a92306e4176c119971fdd19c681d6280322a
barnacle boy <68893555+ixzy@users.noreply.github.com>
2024-08-13 21:56:12 -0400

    Update sniff.ps1

--- sniff.ps1
+++ sniff.ps1
@@ -1,5 +1,5 @@
 # please note this is so basic.
-function cnc {
+function ips {
 $ip = @()
 4..1 |ForEach-Object {
  $ip += Get-Random -Minimum 1 -Maximum 255
@@ -8,7 +8,7 @@ $ip = @()
 }
 
 1..99999999 | ForEach-Object {
-  $ip = cnc
+  $ip = ips
   if(Test-Connection -ComputerName $ip -Count 1 -quiet){
    write-output "$ip Alive"
    } 
